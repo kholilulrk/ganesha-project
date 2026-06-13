c30eba57a04889db55d8feccc7e562b4b2e3f0af
pradanarizaldi84 <pradanarizaldi84@gmail.com>
2026-06-13 09:51:42 +0700

    remove: hapus folder mobile (Flutter app)

--- mobile/android/app/src/main/kotlin/com/landev/ganeshaproject/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.landev.ganeshaproject
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity()
--- mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:website_mobile/main.dart';
-
-void main() {
-  testWidgets('App loads', (WidgetTester tester) async {
-    await tester.pumpWidget(const WebsiteApp());
-    expect(find.text('Ganesha Energi'), findsOneWidget);
-  });
-}
